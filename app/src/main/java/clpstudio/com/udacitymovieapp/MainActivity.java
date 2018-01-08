392ad7dd5aefc4b8e580b73308c96d28c547e986
Clapa Lucian <lucian@dealdash.com>
2018-01-08 21:06:14 +0200

    initial commit

---
+++ app/src/main/java/clpstudio/com/udacitymovieapp/MainActivity.java
@@ -0,0 +1,13 @@
+package clpstudio.com.udacitymovieapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
